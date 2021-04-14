ca304a0334b2538722afe4196cbc85cab813a833
adinacirstea98 <adinacirst@gmail.com>
2021-04-14 14:21:39 +0300

    implemented tasks for colocviu 1 eim

---
+++ PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/general/Constants.java
@@ -0,0 +1,12 @@
+package ro.pub.cs.systems.eim.practicaltest01.general;
+
+public interface Constants {
+    final public static String LEFT_COUNT = "leftCount";
+    final public static String RIGHT_COUNT = "rightCount";
+
+    final public static String NUMBER_OF_CLICKS = "numberOfClicks";
+    final public static int SECONDARY_ACTIVITY_REQUEST_CODE = 1;
+
+    final public static String FIRST_NUMBER = "firstNumber";
+    final public static String SECOND_NUMBER = "secondNumber";
+}
